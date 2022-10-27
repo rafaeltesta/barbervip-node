73aabc76ae5d4701e9ac8050668b3e32d3061165
Rafael Testa <rafaelmax.testa@gmail.com>
2022-10-26 22:49:32 -0300

    adicionado rota barbeiro

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,26 @@ const routes = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+import BarbeiroController  from './controllers/BarbeiroController'
+
+
+//Criar barbeiro
+routes.post("/barbeiro", BarbeiroController.create);
+
+//Consultar barbeiro
+routes.get("/barbeiro", BarbeiroController.get);
+
+//Deletar barbeiro
+routes.delete("/barbeiro/:cdBarbeiro", BarbeiroController.delete);
+
+//Consultar barbeiro por codigo
+routes.get("/barbeiro.details/:cdBarbeiro", BarbeiroController.details);
+
+//Editar barbeiro
+routes.put("/barbeiro", BarbeiroController.update);
+
+
+
 module.exports = routes;
+
+
