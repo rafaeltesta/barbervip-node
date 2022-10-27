652de3c9a0b5a6f33a67574492d1d2cbefd6756a
Rafael Testa <rafaelmax.testa@gmail.com>
2022-10-26 23:03:46 -0300

    ajuste na rota

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-import BarbeiroController  from './controllers/BarbeiroController'
+const BarbeiroController = require('./controllers/BarbeiroController')
 
 
 //Criar barbeiro
